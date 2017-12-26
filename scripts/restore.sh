c37580f76a8e463ac0cf4f84b0be9a8fb45847c6
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2017-12-26 21:48:16 +0100

    Disable SSL checks during database restore
    The way media imports work is by downloading the referenced media over
    HTTP. In case we have a self-signed certificate (which we do), the
    download fails silently.

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -16,9 +16,10 @@ else
     die "Unable to find tar.gz archive to restore"
 fi
 
-case "$(find /srv/ -name htdocs|wc -l)" in
+find_cmd="find /srv/ -type d -name htdocs"
+case "$($find_cmd|wc -l)" in
     1)
-        htdocs="$(find /srv/ -name htdocs)" ;;
+        htdocs="$($find_cmd)" ;;
     0)
         die "Unable to find htdocs directory" ;;
     *)
@@ -30,6 +31,17 @@ set -e -x
 cd "$(dirname "$tgz_path")"
 
 tar -zxvf "$tgz_path" wordpress.xml
+cat > import_no_ssl.php <<"EOF"
+<?php
+/**
+ * Disable SSL checks during "wp import"
+ */
+WP_CLI::add_wp_hook('http_request_args', function ( $args, $url ) {
+    error_log("http_request_args for $url");
+	$args['sslverify'] = false;
+	return $args;
+}, 10, 2);
+EOF
 tar -C"$htdocs" -zxvf "$tgz_path" wp-content/
-wp --path="$htdocs" import --authors=skip wordpress.xml
+wp --path="$htdocs" import --require="./import_no_ssl.php" --authors=skip wordpress.xml
 wp --path="$htdocs" media regenerate --yes
